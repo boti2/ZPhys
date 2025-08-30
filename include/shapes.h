107b4a031ddcc73bb0b079b8e6d973938e83fce6
boti2 <86015141+boti2@users.noreply.github.com>
2025-08-30 14:45:50 +0300

    Update shapes.h

--- include/shapes.h
+++ include/shapes.h
@@ -1,18 +1,22 @@
 #pragma once
 #include <vector>
 #include <ZLib/vec.h>
+#include <cassert>
 
 namespace ZPhys {
-    struct Circle { Vec2 pos, vel; double radius; };
-    struct Triangle { Vec2 verts[3]; Vec2 vel; };
+    struct Triangle {
+        Vec2 verts[3];
+    };
+
+    struct Circle {
+        Vec2 pos;
+        double radius;
+    };
+
     struct Polygon {
         std::vector<Vec2> verts;
-        Vec2 vel;
         std::vector<Triangle> triangles;
+
+        void addLoop(const std::vector<Vec2> &loop);
     };
-    
-    double polygonArea(const std::vector<Vec2>& verts);
-    void ensureCCW(std::vector<Vec2>& verts);
-    bool isDegenerate(const std::vector<Vec2>& verts);
-    bool isSelfIntersecting(const std::vector<Vec2>& verts);
 }
